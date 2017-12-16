eb03db033b06eed7522e0bc58f3c72e86e3a2aee
T.J. Gaffney <gaffney.tj@gmail.com>
2017-12-16 08:36:47 -0500

    Moved HMMParams class and implemented tests.

--- src/axelrod_dojo/__init__.py
+++ src/axelrod_dojo/__init__.py
@@ -1,5 +1,6 @@
 from .version import __version__
 from .archetypes.fsm import FSMParams
+from .archetypes.hmm import HMMParams
 from .archetypes.gambler import GamblerParams
 from .algorithms.genetic_algorithm import Population
 from .utils import (prepare_objective,
